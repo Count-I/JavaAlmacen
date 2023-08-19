095f17a4bab9900cfa496c652e1ff4ef187672f9
count <santiagoosorio310@gmail.com>
2023-08-19 15:09:00 -0500

    create abstract class Persona

--- src/main/java/com/almacen/src/model/Persona.java
+++ src/main/java/com/almacen/src/model/Persona.java
@@ -1,2 +1,74 @@
-package com.almacen.src.model;public class Persona {
+package com.almacen.src.model;
+
+public abstract class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected String documento;
+    protected String direccion;
+    protected String telefono;
+    private TipoPersona tipoPersona;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String documento, String direccion, String telefono, TipoPersona tipoPersona) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipoPersona = tipoPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", documento='" + documento + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", tipoPersona=" + tipoPersona +
+                '}';
+    }
 }
