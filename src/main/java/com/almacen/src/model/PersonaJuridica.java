6ac6a980f641ece408166e12bc03d3b079b2e58f
count <santiagoosorio310@gmail.com>
2023-08-19 15:10:56 -0500

    create Persona sub-classes

--- src/main/java/com/almacen/src/model/PersonaJuridica.java
+++ src/main/java/com/almacen/src/model/PersonaJuridica.java
@@ -1,2 +1,17 @@
-package com.almacen.src.model;public class PersonaJuridica {
+package com.almacen.src.model;
+
+public class PersonaJuridica extends Persona{
+    private String nit;
+
+    public PersonaJuridica(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
 }
--- src/main/java/com/almacen/src/model/PersonaNatural.java
+++ src/main/java/com/almacen/src/model/PersonaNatural.java
@@ -1,2 +1,29 @@
-package com.almacen.src.model;public class PersonaNatural {
+package com.almacen.src.model;
+
+import java.util.Date;
+
+public class PersonaNatural extends Persona{
+    private String email;
+    private Date fechaNacimiento;
+
+    public PersonaNatural(String email, Date fechaNacimiento) {
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
