1d03d32358ae3ce36fcc203690a39b408ba3beef
count <santiagoosorio310@gmail.com>
2023-08-19 15:16:30 -0500

    create Almacen class

--- src/main/java/com/almacen/src/model/Almacen.java
+++ src/main/java/com/almacen/src/model/Almacen.java
@@ -1,2 +1,19 @@
-package com.almacen.src.model;public class Almacen {
+package com.almacen.src.model;
+
+import java.util.List;
+
+public class Almacen {
+    List<Persona> clientes;
+    List<Producto> productos;
+
+    public Almacen() {
+    }
+
+    public List<Persona> getClientes() {
+        return clientes;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
 }
